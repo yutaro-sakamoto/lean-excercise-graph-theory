81d0c96987e30669cb94d2e474e3944c1da8dd71
Yutaro Sakamoto <mail@yutaro-sakamoto.com>
2025-10-02 19:33:00 +0900

    add: xx_edge

--- LeanExcerciseGraphTheory/Playground.lean
+++ LeanExcerciseGraphTheory/Playground.lean
@@ -537,6 +537,25 @@ lemma yv_edge (i : ZMod n) : (dgpg n t nGt1).Adj (y i) (v i) := by
   have h_sym : (dgpg n t nGt1).Adj (v i) (y i) := vy_edge n t nGt1 i
   exact h_sym.symm
 
+lemma xx_edge (i : ZMod n) : (dgpg n t nGt1).Adj (x i) (x (i + 1)) := by
+  simp [dgpg, xx_ne]
+  apply elem_list_to_set
+  apply List.mem_append_left
+  apply List.mem_map.mpr
+  use i
+  constructor
+  · -- i ∈ List.range n を証明
+    have n_ne_zero : n ≠ 0 := n_neq_zero n nGt1
+    have : NeZero n := ⟨n_ne_zero⟩
+    simp [List.mem_flatMap]
+    use i.val
+    constructor
+    · exact i.val_lt
+    · simp
+  · -- s(x i, x (i + 1)) = s(x ↑i, x ↑(i + 1)) を証明
+    simp
+
+
 def xu_walk (i : ZMod n) : SimpleGraph.Walk (dgpg n t nGt1) (x i) (u i) :=
   SimpleGraph.Walk.cons (xu_edge n t nGt1 i) SimpleGraph.Walk.nil
 
